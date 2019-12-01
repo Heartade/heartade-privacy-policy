3d3a5f3a38bcd2e221f84c5a591e4f5301b8b2a1
heartade <soobak0313@gmail.com>
2019-12-02 02:28:10 +0900

    initial commit with sigansim

---
+++ src/home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>개인정보처리방침</h1>
+      </div>
+    );
+  }
+}
---
+++ src/three_sixty_one.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ThreeSixtyOne extends Component {
+  render() {
+    return (
+      <div>
+        <h1>개인정보처리방침</h1>
+      </div>
+    )
+  }
+}
